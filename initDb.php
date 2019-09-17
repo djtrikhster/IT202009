bc21b615364efbf881ac4f9482db03455d907598
djtrikhster <rt436@njit.edu>
2019-09-17 10:28:10 -0400

    updated initdb

--- initDb.php
+++ initDb.php
@@ -1,7 +1,7 @@
 <?php
 #turn error reporting on
 ini_set('display_errors', 1);
-ini_set('display_startup_errors');
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require('conf.php');
