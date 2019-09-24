4589acd536f7b6568444f06d5e29d3eff9d3c538
djtrikhster <rt436@njit.edu>
2019-09-24 11:01:52 -0400

    Pulled new initDB from class github

--- initDB.php
+++ initDB.php
@@ -3,58 +3,33 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-//pull in config.php so we can access the variables from it
+//pull in conf.php so we can access the variables from it
 require('conf.php');
-echo "Loaded Host: " . $host;
+//echo "Loaded Host: " . $host;
 
 $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 
 try{
+	foreach(glob("sql/*.sql") as $filename){
+		//echo $filename;
+		$sql[$filename] = file_get_contents($filename);
+		//echo $sql[$filename];
+	}
+	ksort($sql);
+
+	//connect to DB
 	$db = new PDO($conn_string, $username, $password);
-	echo "Connected";
-	$query = "create table if not exists `TestUsers`(
-		`id` int auto_increment not null,
-		`username` varchar(30) not null unique,
-		`pin` int default 0,
-		PRIMARY KEY (`id`)
-		) CHARACTER SET utf8 COLLATE utf8_general_ci";
-	$stmt = $db->prepare($query);
-	$r = $stmt->execute();
-	echo "<br>" . $r . "<br>";
-
-	$insert_query = "INSERT INTO `TestUsers` (`id`, `username`, `pin`) VALUES ('11', 't23est', '032000')";
-	$stmt = $db->prepare($insert_query);
-	$r = $stmt->execute();
-
-	
-	$randID = rand(1, 1000);
-	$randUser = 'TestUser' . rand(1, 1000);
-	$randPin = rand(1, 1000);
-	echo $randID . ' ' . $randUser . ' ' . $randPin;
-	
-	
-	$insert_rand = "INSERT INTO `TestUsers` (`id`, `username`, `pin`) VALUES ('$randID', '$randUser', '$randPin')";
-	$stmt = $db->prepare($insert_rand);
-	$r = $stmt->execute();
-	echo "<br>" . $r . "<br>";
-	echo "<br />" . 'inserted';
-
-	$insert_query= "INSERT INTO  `TestUsers` (`username`, `pin`) VALUES (:username, :pin)"; 
-	$stmt = $db->prepare($insert_query);
-	$user = "George Washington";
-	$pin = 1234;
-	//$stmt = bindParam(":username" = $user, PDO::PARAM_STR);
-	//$stmt = bindParam(":id" = $pin, PDO::PARAM_INT);
-	//https://stackoverflow.com/questions/26349005/php-warning-pdostatementexecute-sqlstatehy093-invalid-parameter-number
-	$r = $stmt->execute(array(":username"=>$user, ":pin"=>$pin));
-	print_r($stmt->errorInfo());
-
-	$select_query = "SELECT * FROM `TestUsers` WHERE username = :username";
-	$stmt = $db->prepare($select_query);
-	$r = $stmt->execute();
-
-	$result = $stmt->fetch();
-	echo "<br><pre>" . var_export($result, true) . "</pre><br>";
+	//$db->setAttribute(PDO::ATTR_ERRMODE);
+	foreach($sql as $key => $value){
+		echo "<br>Running: " . $key;
+		$stmt = $db->prepare($value);
+		$result = $stmt->execute();
+		$error = $stmt->errorInfo();
+		if($error && $error[0] !== '00000'){
+			echo "<br>Error:<pre>" . var_export($error,true) . "</pre><br>";
+		}
+		echo "<br>$key result: " . ($result>0?"Success":"Fail") . "<br>";
+	}
 	
 
 }
