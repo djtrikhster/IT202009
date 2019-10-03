5e8c56374622a404c234f1456b1d064be4a7d1ce
djtrikhster <rt436@njit.edu>
2019-10-03 09:35:46 -0400

    added <div> elements

---
+++ jsSample.scratch.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+	<!--css and js here -->
+		<script>
+			//allert("Hello World")
+			var myVariable;
+			let myOtherVariable;
+			var domEl = document.getElementsByTagName("BODY")[0];
+			myVariable = prompt("What's your name?");
+			alert("Hello, " + myVariable);
+		</script>
+	</head>
+	<body>
+	<!-- html content here -->
+		<p> It loaded yay! </p>
+		<div style="background-color:green">
+			<p>"the background is green"</p>
+		</div>
+		<div>
+			<p>"added new element" </p>
+	</body>
+</html>
+
